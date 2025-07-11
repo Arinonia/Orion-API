29b4b7b946b4a2b6fed45df3fa773c32bc1ed6df
Arinonia <arinonia.yt@gmail.com>
2025-07-11 13:23:06 +0200

    switch version to 1.0.0

--- src/main/java/fr/orion/api/utils/Constants.java
+++ src/main/java/fr/orion/api/utils/Constants.java
@@ -2,5 +2,5 @@ package fr.orion.api.utils;
 
 public class Constants {
     public static final String APP_NAME = "Orion-API";
-    public static final String APP_VERSION = "0.0.1-SNAPSHOT";
+    public static final String APP_VERSION = "1.0.0";
 }
